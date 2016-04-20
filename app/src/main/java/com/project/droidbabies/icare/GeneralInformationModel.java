592163a6cb1348a075d843ee287cfce6d2d583a8
tajmulalam <taj.cse37@gmail.com>
2016-04-20 12:07:02 +0600

    Manager created

--- app/src/main/java/com/project/droidbabies/icare/GeneralInformationModel.java
+++ app/src/main/java/com/project/droidbabies/icare/GeneralInformationModel.java
@@ -4,5 +4,28 @@ package com.project.droidbabies.icare;
  * Created by Mobile App Develop on 20-4-16.
  */
 public class GeneralInformationModel {
-    private static int generalInfoid;
+    private  int Infoid;
+    private String  title , description,collectedFrom;
+
+    public GeneralInformationModel(int infoid, String title, String description, String collectedFrom) {
+        Infoid = infoid;
+        this.title = title;
+        this.description = description;
+        this.collectedFrom = collectedFrom;
+    }
+
+    public GeneralInformationModel() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCollectedFrom() {
+        return collectedFrom;
+    }
 }
--- app/src/main/java/com/project/droidbabies/icare/MainActivity.java
+++ app/src/main/java/com/project/droidbabies/icare/MainActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+GeneralInformationManager manager;
+    ArrayList<GeneralInformationModel> generalInformationList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager=new GeneralInformationManager(this);
+        generalInformationList=new ArrayList<>();
+        generalInformationList=manager.getGeneralInfos();
+
+        Toast.makeText(MainActivity.this, String.valueOf(generalInformationList.size()), Toast.LENGTH_SHORT).show();
+
     }
 
     public void addNewProfile(View view) {
